60cfda5651bca97f0a90a327fb1d4b2388dbbfe5
Abu <efootball042605@gmail.com>
2026-01-15 16:26:27 +0500

    Python kodi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,28 @@
+import csv
 
+input_file = 'students.csv'
+
+with open(input_file) as f:
+    reader = csv.DictReader(f, fieldnames=['name', 'score'])
+    next(reader) 
+
+    students = sorted(
+        reader,
+        key=lambda ball: int(ball['score']),
+    )
+
+output_file = 'rating.csv'
+
+with open(output_file, 'w') as f:
+    writer = csv.DictWriter(f, fieldnames = ['rank', 'name', 'score'])
+
+    writer.writeheader()
+
+    for i, student in enumerate(students, start=1):
+        writer.writerow({
+            'rank': i,
+            'name': student['name'],
+            'score': student['score']
+        })
+
+print('rating.csv muvaffaqiyatli yaratildi')
